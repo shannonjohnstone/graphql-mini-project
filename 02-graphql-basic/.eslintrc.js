1f861a4047af51ee4d9c2533d6cca3f388c9f00b
shannonjohnstone <shannon@shannonjohnstone.com.au>
2019-03-03 11:44:51 +1100

    add basic babel setup. src folder, fix eslint rules

--- 02-graphql-basic/.eslintrc.js
+++ 02-graphql-basic/.eslintrc.js
@@ -4,11 +4,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:node/recommended',
     'plugin:prettier/recommended',
-    'plugin:jest/recommended',
   ],
   rules: {
-    'react/jsx-filename-extension': 0,
-    'react/jsx-one-expression-per-line': 0, // Conflicting with prettier
     'object-curly-spacing': ['error', 'always'],
     'key-spacing': ['error', { 'afterColon': true }],
     'semi': 'error',
