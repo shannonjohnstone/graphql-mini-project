e7add58b8e43de04258209d8b36f1ae45777cd98
shannonjohnstone <shannon@shannonjohnstone.com.au>
2019-03-03 14:41:38 +1100

    add nodemon, update eslint rule, new types and resolvers

--- 02-graphql-basic/.eslintrc.js
+++ 02-graphql-basic/.eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     'object-curly-spacing': ['error', 'always'],
     'key-spacing': ['error', { 'afterColon': true }],
     'semi': 'error',
-    'quotes': ['error', 'single'],
+    'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
     'max-len': ['error', 80, {
       "ignoreComments": true,
       "ignoreStrings": true
--- 02-graphql-basic/src/index.js
+++ 02-graphql-basic/src/index.js
@@ -4,6 +4,9 @@ import { GraphQLServer } from 'graphql-yoga';
 const typeDefs = `
   type Query {
     hello: String!
+    name: String!
+    location: String!
+    bio: String!
   }
 `;
 
@@ -13,9 +16,18 @@ const resolvers = {
     hello() {
       return 'This is the first query!';
     },
+    name() {
+      return 'Dustin';
+    },
+    location() {
+      return 'Sydney';
+    },
+    bio() {
+      return `I'm a dog`;
+    },
   },
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 
-server.start(() => console.log('Server is up'));
+server.start(() => console.log('Server is up')); // eslint-disable-line
